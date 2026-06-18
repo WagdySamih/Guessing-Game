f9a8f59947312f768234ac4fec69b0eeebea719f
Wagdy Samih <WagdySamih787@gmail.com>
2026-06-18 20:07:01 +0300

    Init commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text
+        style={{
+          color: "blue",
+          backgroundColor: "yellow",
+          padding: 13,
+          borderRadius: 12,
+          fontWeight: "500",
+        }}
+      >
+        Hello Raven from you best friend Killua
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
